0c32e2ce02b66645185d20d6fa27842c8e01eeae
Jonathan LYS <j21lys@sl-mee-br-501.imta.fr>
2025-11-09 22:38:04 +0100

    dedup port

---
+++ .scripts/launch_bis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# source $JOME/d3p2/.venv/bin/activate
+
+ROOT=$JOME/d3p2/src
+
+cd $ROOT
+export PYTHONPATH=$ROOT:$PYTHONPATH
+
+OMP_NUM_THREADS=1 torchrun --rdzv_endpoint=localhost:29400 --nproc_per_node=gpu main.py config=_default.yaml $1
