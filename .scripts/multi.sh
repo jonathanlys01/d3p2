8e3417bc1796367092cef8745425476d350249e5
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-12 16:10:24 +0100

    modify multi param to greedy_map for faster exp

--- .scripts/multi.sh
+++ .scripts/multi.sh
@@ -7,5 +7,5 @@ ROOT=$JOME/d3p2/src
 cd $ROOT
 export PYTHONPATH=$ROOT:$PYTHONPATH
 
-OMP_NUM_THREADS=1 torchrun --nproc_per_node=gpu single_run_mdlm.py --config=_default.yaml method=dpp transversal=False initial_mask_ratio=0.9 single_init=True
+OMP_NUM_THREADS=1 torchrun --nproc_per_node=gpu single_run_mdlm.py --config=_default.yaml method=greedy_map transversal=True initial_mask_ratio=0.9 single_init=True
 # $1 # > out_debug.log 2>&1
