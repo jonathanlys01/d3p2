3a3896d7ccae5d17b89147490cf0328e573ba1ec
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-12 10:09:10 +0100

    increase gr size in llada exp

--- .scripts/llada_main.sh
+++ .scripts/llada_main.sh
@@ -12,9 +12,9 @@ set -ex
 
 
 MASTER_PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-OMP_NUM_THREADS=1 torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT exps/llada.py --config=_default.yaml model=llada cat_temperature=1 cfg_scale=0.33 guidance_end=85 _w_interaction=10 group_size=4 "$@"
+OMP_NUM_THREADS=1 torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT exps/llada.py --config=_default.yaml model=llada cat_temperature=1 cfg_scale=0.33 guidance_end=85 _w_interaction=10 group_size=8 "$@"
 
 MASTER_PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
-OMP_NUM_THREADS=1 torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT exps/llada.py --config=_default.yaml model=llada cat_temperature=1 cfg_scale=0.33 guidance_end=85 _w_interaction=0 group_size=4 "$@"
+OMP_NUM_THREADS=1 torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT exps/llada.py --config=_default.yaml model=llada cat_temperature=1 cfg_scale=0.33 guidance_end=85 _w_interaction=0 group_size=8 "$@"
 
 echo Done
