0b3fdb2f2261700043f0183d94a3eed2bec64d59
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-22 23:42:33 +0100

    minor edit

--- .scripts/div_bs.sh
+++ .scripts/div_bs.sh
@@ -7,4 +7,4 @@ export PYTHONPATH=$ROOT:$PYTHONPATH
 export OMP_NUM_THREADS=1
 
 
-torchrun --nproc_per_node=gpu exps/sweeps/div_bs.py --config=_default.yaml n_trials=200 ppl_model_id=gpt2-large n_groups=2 group_size=8
+torchrun --nproc_per_node=gpu exps/sweeps/div_bs.py --config=_default.yaml n_trials=200 ppl_model_id=gpt2-large n_groups=2 group_size=8 method=diverse_beam
