70483113aa321509b49e1e092a024c2ba47084ea
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-28 22:35:34 +0100

    reduce deadline scripts

--- .deadline/cfg_base.sh
+++ .deadline/cfg_base.sh
@@ -14,6 +14,6 @@ shift
 CFG_VAL=$(python -c "import numpy as np; vals = np.logspace(np.log10(1), np.log10(3), num=10); print(vals[$i])")
 
 set -ex
-python exps/cfg_exp.py --config=_default.yaml model=llada n_groups=16 group_size=1 method=baseline cfg_scale=$CFG_VAL "$@"
+python exps/cfg_exp.py --config=_default.yaml model=llada n_groups=8 group_size=1 method=baseline cfg_scale=$CFG_VAL "$@"
 
 echo "Job ended at $(date)"
