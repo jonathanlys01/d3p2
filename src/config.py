252586beff275fbc506a979288dae1f5de70cf22
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-28 02:19:14 +0100

    cfg change tau

--- src/config.py
+++ src/config.py
@@ -106,7 +106,7 @@ class Config:
     qa_dataset: str = "truthful_qa"  # "truthful_qa" or "commonsense_qa"
     qa_dataset_len: int = -1  # number of samples to use from qa_dataset (-1 for all)
     truthful_qa_path: str = "truthfulqa/truthful_qa"
-    commonsense_qa_path: str = "commonsense_qa"
+    commonsense_qa_path: str = "tau/commonsense_qa"
 
     # cache
     cache_dir: str = CACHE_DIR
