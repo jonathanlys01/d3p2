4a74e48918bde58e64953550cfebf127e3e2828b
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-28 22:47:43 +0100

    fix deadline scripts

--- .deadline/main_baseline.sh
+++ .deadline/main_baseline.sh
@@ -10,6 +10,6 @@ shift
 MASTER_PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 set -ex
-torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT exps/baseline_llada.py --config=_default.yaml method=baseline n_groups=16 group_size=1 qa_dataset=$DS "$@"
+torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT exps/baseline_llada.py --config=_default.yaml model=llada method=baseline n_groups=16 group_size=1 qa_dataset=$DS "$@"
 
 echo "Job ended at $(date)"
--- .deadline/main_map.sh
+++ .deadline/main_map.sh
@@ -10,6 +10,6 @@ shift
 MASTER_PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 set -ex
-torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT single_run_llada.py --config=_default.yaml method=greedy_map _w_interaction=8 cfg_scale=2.5 n_groups=4 group_size=4 guidance_end=64 qa_dataset=$DS "$@"
+torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT single_run_llada.py --config=_default.yaml model=llada method=greedy_map _w_interaction=8 cfg_scale=2.5 n_groups=4 group_size=4 guidance_end=64 qa_dataset=$DS "$@"
 
 echo "Job ended at $(date)"
