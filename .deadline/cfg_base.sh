3d0a60352c836a2a96d6cd6c1077c065cb42e675
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-28 21:47:20 +0100

    add all deadline scripts

--- .deadline/cfg_base.sh
+++ .deadline/cfg_base.sh
@@ -11,9 +11,9 @@ export PYTHONPATH=$ROOT:$PYTHONPATH
 i=$1
 shift
 
-CFG_VAL=$(python -c "import numpy as np; vals = np.logspace(np.log10(0.5), np.log10(2.5), num=6); print(vals[$i])")
-echo "Launching CFG=$CFG_VAL"
+CFG_VAL=$(python -c "import numpy as np; vals = np.logspace(np.log10(1), np.log10(3), num=6); print(vals[$i])")
 
+set -ex
 python exps/cfg_exp.py --config=_default.yaml model=llada n_groups=16 group_size=1 method=baseline cfg_scale=$CFG_VAL "$@"
 
-#TODO: verify
+echo "Job ended at $(date)"
--- .deadline/cfg_map.sh
+++ .deadline/cfg_map.sh
@@ -12,6 +12,8 @@ i=$1
 shift
 
 CFG_VAL=$(python -c "import numpy as np; vals = np.logspace(np.log10(1), np.log10(3), num=6); print(vals[$i])")
-echo "Launching CFG=$CFG_VAL"
 
+set -ex
 python exps/cfg_exp.py --config=_default.yaml model=llada n_groups=4 group_size=4 method=greedy_map _w_interaction=8 cfg_scale=$CFG_VAL "$@"
+
+echo "Job ended at $(date)"
