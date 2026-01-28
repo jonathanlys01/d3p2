f9f497a177cb2cbdf14cbfbc790f8baa5c0ebb19
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-01-28 19:05:49 +0100

    interac change bs

--- .deadline/llada_inter_2.sh
+++ .deadline/llada_inter_2.sh
@@ -35,7 +35,9 @@ for w_inter in "${INTERACTIONS[@]}"; do
         model=llada \
         n_runs=$N_RUNS \
         _w_interaction=$w_inter \
-        method=greedy_map
+        method=greedy_map \
+        n_groups=4 \
+        group_size=4
     set +ex
     
     OUTPUT=$(ls -t $ROOT/results/exp-*.json | head -n 1)
