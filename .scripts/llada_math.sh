2ec06ba4e2e3bed0a279cbcfb1e724539265023d
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-03-09 15:20:53 +0100

    (hp) match configuration

--- .scripts/llada_math.sh
+++ .scripts/llada_math.sh
@@ -2,14 +2,16 @@
 
 ROOT=$(pwd)/src/d5p4
 
+N_QUESTIONS=100
+
 cd $ROOT
 export OMP_NUM_THREADS=1
 MASTER_PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
 set -ex
 
-torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT llada_math.py --config=_default.yaml model=llada qa_n_shots=4 n_groups=2 group_size=2
+torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT llada_math.py --config=_default.yaml model=llada qa_n_shots=4 n_groups=2 group_size=2 qa_dataset_len=$N_QUESTIONS
 
-torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT llada_math.py --config=_default.yaml model=llada qa_n_shots=4 n_groups=1 group_size=4
+torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT llada_math.py --config=_default.yaml model=llada qa_n_shots=4 n_groups=1 group_size=4 qa_dataset_len=$N_QUESTIONS
 
-torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT llada_math.py --config=_default.yaml model=llada qa_n_shots=4 n_groups=4 group_size=1 method=baseline
+torchrun --nproc_per_node=gpu --master_port=$MASTER_PORT llada_math.py --config=_default.yaml model=llada qa_n_shots=4 n_groups=4 group_size=1 method=baseline qa_dataset_len=$N_QUESTIONS
