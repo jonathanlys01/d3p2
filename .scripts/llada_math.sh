38a6f8ff4eb32d1beef8e527bb1e637715969483
jonathanlys01 <jonathanlystahiti@gmail.com>
2026-04-05 16:35:31 +0200

    (exp) sweep w_inter

--- .scripts/llada_math.sh
+++ .scripts/llada_math.sh
@@ -2,7 +2,7 @@
 
 ROOT=$(pwd)/src/d5p4
 
-N_QUESTIONS=${N_QUESTIONS:-100}
+N_QUESTIONS=${N_QUESTIONS:-150}
 NPROC=${NPROC:-gpu}
 
 cd $ROOT
@@ -11,11 +11,14 @@ export OMP_NUM_THREADS=1
 
 COMMON_ARGS=(
   --config=_default.yaml
+  minimal_log=true
   model=llada
   qa_n_shots=4
   qa_dataset_len=$N_QUESTIONS
   cat_temperature=1.0
-  remasking=low_confidence
+  #remasking=low_confidence
+  remasking=selection_temperature
+  selection_temperature=0.1
   logits_eos_inf=False
   cfg_scale=2.0
   llada_steps=256
@@ -30,36 +33,8 @@ run_llada_math() {
   torchrun --nproc_per_node=$NPROC --master_port=$MASTER_PORT llada_math.py "${COMMON_ARGS[@]}" "$@"
 }
 
-list_w_interaction=(0.0 1.0 2.5 5.0 10.0)
+list_w_interaction=(50.0 75.0 100.0 120.0)
 set -ex
-
-run_llada_math \
-    method=diverse_beam \
-    n_groups=2 \
-    group_size=2 \
-    subsample_end=128 \
-    comment="derisk" \
-    qa_dataset_len=50 \
-    _diversity_alpha=20.
-
-exit
-
-# derisk
-run_llada_math \
-    method=greedy_map \
-    n_groups=2 \
-    group_size=2 \
-    subsample_end=128 \
-    comment="derisk" \
-    qa_dataset_len=5
-
-
-run_llada_math \
-    method=baseline \
-    n_groups=4 \
-    group_size=1 \
-    comment="Independent pure baseline"
-
 
 for w_interaction in "${list_w_interaction[@]}"; do
   run_llada_math \
@@ -67,6 +42,6 @@ for w_interaction in "${list_w_interaction[@]}"; do
     n_groups=2 \
     group_size=2 \
     subsample_end=128 \
-    comment="D5P4 pure partial @ w_inter=${w_interaction}" \
+    comment="D5P4 pure partial @ w_inter=${w_interaction}, sel_temp=0.1" \
     _w_interaction=$w_interaction
 done
